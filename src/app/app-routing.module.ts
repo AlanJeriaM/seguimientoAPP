189679f6ae06f3f717006bd4f1b42ca9631ef450
Alan Jeria Medina <alanjeria01@gmail.com>
2024-07-23 17:31:13 -0400

    actualizacion de app-routing.module.ts, app.module.ts y ademas se creo las rutas del visitor, visitor-routing.module.ts y visitor.module.ts, y tambien se dejo listo el componente main, para en la rama siguiente trabajar con los demas componentes de visitor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'visitor',
+    loadChildren: () => import('./visitor/visitor.module').then(m => m.VisitorModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+    //canActivate: [ClientGuard], AQUI TENGO QUE APRENDER A APLICAR LOS GUARDS PARA PROTEGER LAS RUTAS DEL LOGIN.
+    //canLoad: [ClientGuard]
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    //canActivate : [AdminGuard],
+    //canLoad : [AdminGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'visitor'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
