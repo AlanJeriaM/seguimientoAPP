1dc8b287afb4766e9992aabbabeccbdf218757cd
Alan Jeria Medina <alanjeria01@gmail.com>
2024-07-23 01:03:06 -0400

    estructura de la app - mas adelante ver el core

---
+++ src/app/admin/pages/view-deleted-users/view-deleted-users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-deleted-users',
+  templateUrl: './view-deleted-users.component.html',
+  styleUrl: './view-deleted-users.component.css'
+})
+export class ViewDeletedUsersComponent {
+
+}
