0f125b859ae0bf86481c1c335b4dee08f978a8f8
Alan Jeria Medina <alanjeria01@gmail.com>
2024-07-23 21:24:30 -0400

    update visitor-routing.module.ts y creacion de las rutas de admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { ViewUsersComponent } from './pages/view-users/view-users.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewDeletedUsersComponent } from './pages/view-deleted-users/view-deleted-users.component';
+import { CreateEncuestaComponent } from './pages/create-encuesta/create-encuesta.component';
+import { DashboardAdminComponent } from './pages/dashboard-admin/dashboard-admin.component';
 
 const routes: Routes = [
   {
+      path: '',
+      component: MainComponent,
+      children: [
+        { path: 'home', component: HomeComponent },
+        { path: 'view-users', component: ViewUsersComponent },
+        { path: 'view-deleted-users', component: ViewDeletedUsersComponent },
+        { path: 'create-encuesta', component: CreateEncuestaComponent},
+        { path: 'dashboard', component: DashboardAdminComponent},
+        { path: '**', redirectTo: 'home' }
+      ],
+    }
 
-  }
     ]
 
 @NgModule({
--- src/app/visitor/visitor-routing.module.ts
+++ src/app/visitor/visitor-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
@@ -9,14 +8,10 @@ const routes: Routes = [
 
     path: '', component: MainComponent,
     children: [
-      { path: 'home', component: HomeComponent },
       { path: 'login', component: LoginComponent },
-      { path: '**', redirectTo: 'home' },
-
+      { path: '**', redirectTo: 'login' },
 
     ]
-
-
   }
     ];
 
