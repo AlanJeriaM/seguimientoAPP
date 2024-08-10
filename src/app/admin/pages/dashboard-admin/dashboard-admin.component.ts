2029c0ffdb37cca3b509c395fe251133a63af46b
Alan Jeria Medina <alanjeria01@gmail.com>
2024-08-09 23:46:18 -0400

    desinstalando bootstrap trae conflictos con primeNG

--- src/app/admin/pages/dashboard-admin/dashboard-admin.component.ts
+++ src/app/admin/pages/dashboard-admin/dashboard-admin.component.ts
@@ -13,52 +13,5 @@ export class DashboardAdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.chartData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Connections',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: '#42A5F5',
-          tension: .4
-        },
-        {
-          label: 'Messages',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          borderColor: '#FFA726',
-          tension: .4
-        }
-      ]
-    };
-
-    this.chartOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: '#495057'
-          }
-        }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        }
-      }
-    };
   }
 }
