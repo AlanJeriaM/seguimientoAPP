90a0b3d8f47eaa0a7a6f8c0c999920121d0eac63
Alan Jeria Medina <alanjeria01@gmail.com>
2024-10-22 18:21:30 -0300

    se realizo el front de usuarios activos, eliminados, listado de encuestas, todo eso por el lado del admin, y por el lado del usuario, se creo la parte de encuestas completadas.

--- src/app/admin/pages/view-deleted-users/view-deleted-users.component.ts
+++ src/app/admin/pages/view-deleted-users/view-deleted-users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EstandardDticService } from '../../../core/services/dtic/estandard-dtic.service';
 
 @Component({
   selector: 'app-view-deleted-users',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './view-deleted-users.component.css'
 })
 export class ViewDeletedUsersComponent {
+
+  constructor(private estandardDticService: EstandardDticService) {}
+
+  registros: any[] = [];
+
+  ngOnInit() {
+    this.registros = this.estandardDticService.getRegistros();
+  }
+
+  comboRandomA = [
+    { label: 'randomA_0', value: 'randomA_0' },
+    { label: 'randomA_1', value: 'randomA_1' },
+  ];
+  comboRandomB = [
+    { label: 'randomB_0', value: 'randomB_0' },
+    { label: 'randomB_1', value: 'randomB_1' },
+    { label: 'randomB_2', value: 'randomB_2' },
+    { label: 'randomB_3', value: 'randomB_3' },
+    { label: 'randomB_4', value: 'randomB_4' },
+  ];
 
 }
--- src/app/admin/pages/view-users/view-users.component.ts
+++ src/app/admin/pages/view-users/view-users.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../../core/services/usuario/usuario.service';
+import { EstandardDticService } from '../../../core/services/dtic/estandard-dtic.service';
+
 
 @Component({
   selector: 'app-view-users',
   templateUrl: './view-users.component.html',
-  styleUrl: './view-users.component.css'
+  styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent {
+
+  constructor(private estandardDticService: EstandardDticService) {}
+
+  registros: any[] = [];
+
+  ngOnInit() {
+    this.registros = this.estandardDticService.getRegistros();
+  }
+
+  comboRandomA = [
+    { label: 'randomA_0', value: 'randomA_0' },
+    { label: 'randomA_1', value: 'randomA_1' },
+  ];
+  comboRandomB = [
+    { label: 'randomB_0', value: 'randomB_0' },
+    { label: 'randomB_1', value: 'randomB_1' },
+    { label: 'randomB_2', value: 'randomB_2' },
+    { label: 'randomB_3', value: 'randomB_3' },
+    { label: 'randomB_4', value: 'randomB_4' },
+  ];
 
 }
